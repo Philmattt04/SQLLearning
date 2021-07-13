d363f6e490a5fef51e93954d91ead8e845c29add
Philmattt04 <87322361+Philmattt04@users.noreply.github.com>
2021-07-13 10:08:31 -0500

    Add files via upload
    This code shows the concept of receiving the total number of rows in a specific column on SQL.

---
+++ users.sql
@@ -0,0 +1,4 @@
+-- This collects the data from a table, which is called "users"
+select * from users;
+-- Using the query "count()", this takes the total number of rows in the column, which is "id"
+select count(id) from users;
